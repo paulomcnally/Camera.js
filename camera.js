d7549f2cf834ca77a518d28b4546647cd9aaad0b
Paulo McNally <paulomcnally@gmail.com>
2014-05-24 02:46:06 -0600

    validation added

--- camera.js
+++ camera.js
@@ -1,4 +1,5 @@
 var hilink = require('hilink');
+var config = require('./config');
 
 
 hilink.listInbox(function( response ){
@@ -7,7 +8,28 @@ hilink.listInbox(function( response ){
 
         response["response"]["Messages"].forEach( function( sms ){
 
-            console.log( sms["Message"][0]["Content"][0] );
+            var id = sms["Message"][0]["Index"][0];
+
+            var phone = sms["Message"][0]["Phone"][0];
+
+            var text = sms["Message"][0]["Content"][0];
+
+
+            if( config.numbers.indexOf( phone ) ){
+
+
+
+                hilink.delete( id, function( status ){
+
+                    console.log( status );
+
+                    console.log( text );
+
+                });
+
+            }
+
+
 
         });
 
---
+++ config.js
@@ -0,0 +1,9 @@
+var Config = function(){
+
+    var self = this;
+
+    self.numbers = [ "+50582406543" ];
+
+}
+
+models.exports = new Config();
